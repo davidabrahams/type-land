0a0817ec4bdd598d716d86acde018a47f1e55520
David Abrahams <da.j.abrahams@gmail.com>
2019-11-15 11:02:32 -0800

    add some nice syntax for constructing lists

--- src/main/scala/TypedList.scala
+++ src/main/scala/TypedList.scala
@@ -16,20 +16,24 @@ object List {
   type Aux[A0, N0 <: Nat] = List[A0] { type N = N0 }
 }
 
-sealed trait Empty extends List[Nothing]
+sealed trait Empty extends List[Nothing] {
+  type N = _0
+  def ::[A](v: A) = Cons(v, this)
+}
 case object Empty extends Empty
 
 final case class Cons[A, P <: Nat](head: A, tail: List.Aux[A, P])
-    extends List[A] {
+    extends List[A] { cons =>
+  def ::(v: A): Cons[A, N] { type N = Succ[cons.N] } = Cons(v, this)
   type N = Succ[P]
 }
 
 object Main {
   def main(args: Array[String]): Unit = {
     val list = Empty
-    val l1 = Cons(2, Cons(0, Empty))
-    val l2 = Cons(1, Cons(1, Cons(5, Empty)))
-    val l3 = Cons(1, Cons(5, Empty))
+    val l1 = 2 :: 0 :: Empty
+    val l2 = 1 :: 1 :: 5 :: Empty
+    val l3 = 1 :: 5 :: Empty
     val func = (a: Int, b: Int) => a + b
     println(List.add(l1, l3, func))
     println("Failure!")
