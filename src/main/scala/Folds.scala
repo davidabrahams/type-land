81493c9337e4bd645e557cac3c1fcb838421e313
David Abrahams <da.j.abrahams@gmail.com>
2019-11-14 22:54:27 -0800

    initial commit

---
+++ src/main/scala/Folds.scala
@@ -0,0 +1,67 @@
+object Peano {
+
+  sealed trait Nat {
+    // a type level function that folds over ints
+    // we must provide a two argument type constructor to fold each int
+    // into Up
+    type FoldR[Init <: Acc, Func <: FoldFunc[Acc, Nat], Acc] <: Acc
+  }
+
+  // a little weird, FoldFunc[A, B] is a type with a type-function (A, B) => B
+  // FoldFunc is contravarient Val, as it is the input to the function
+  // FoldFunc is invariant in Acc, because Acc is both the input type
+  // (requiring contravariance) and the output type (requiring covariance)
+  sealed trait FoldFunc[Acc, -Val] {
+    type Apply[A <: Acc, V <: Val] <: Acc
+  }
+
+  sealed trait _0 extends Nat {
+    // base case
+    type FoldR[Init <: Acc, Func <: FoldFunc[Acc, Nat], Acc] = Init
+  }
+  sealed trait Succ[N <: Nat] extends Nat {
+    type FoldR[Init <: Acc, Func <: FoldFunc[Acc, Nat], Acc] =
+      Func#Apply[N#FoldR[Init, Func, Acc], Succ[N]]
+  }
+
+  type Increment = FoldFunc[Nat, Nat] {
+    type Apply[A <: Nat, V <: Nat] = Succ[A]
+  }
+  type Add[A <: Nat, V <: Nat] = V#FoldR[A, Increment, Nat]
+  type AddBy[By <: Nat] = FoldFunc[Nat, Nat] {
+    type Apply[A <: Nat, V <: Nat] = Add[A, By]
+  }
+  type Mult = FoldFunc[Nat, Nat] {
+    type Apply[A <: Nat, V <: Nat] = V#FoldR[_0, AddBy[A], Nat]
+  }
+  // fact(a) == 1 * 2 * 3 * ... * a
+  type Fact[V <: Nat] = V#FoldR[_1, Mult, Nat]
+
+  type _1 = Succ[_0]
+  type _2 = Succ[_1]
+  type _3 = Succ[_2]
+  type _4 = Succ[_3]
+  type _5 = Succ[_4]
+  type _6 = Succ[_5]
+  type _7 = Succ[_6]
+  type _8 = Succ[_7]
+  type _9 = Succ[_8]
+  type _10 = Succ[_9]
+  type _11 = Succ[_10]
+  type _12 = Succ[_11]
+
+  def main(args: Array[String]): Unit = {
+    implicitly[Add[_1, _2] =:= _3]
+    implicitly[Add[_1, _4] =:= _5]
+    implicitly[Add[_3, _4] =:= _7]
+    implicitly[Mult#Apply[_1, _4] =:= _4]
+    implicitly[Mult#Apply[_2, _3] =:= _6]
+    implicitly[Mult#Apply[_3, _3] =:= _9]
+    implicitly[Mult#Apply[_3, _3] =:= _9]
+    implicitly[Fact[_0] =:= _1]
+    implicitly[Fact[_1] =:= _1]
+    implicitly[Fact[_2] =:= _2]
+    implicitly[Fact[_3] =:= _6]
+    println("Success!")
+  }
+}
